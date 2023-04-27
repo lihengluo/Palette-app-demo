de467b8d390326e7b32f8a161e492e12c60287d1
shimmer147 <wailiheng@gmail.com>
2023-04-27 18:24:40 +0800

    add the c++ code

---
+++ app/src/main/cpp/build/CMakeFiles/monet.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/Eighthalf/AndroidStudioProjects/ComposeTuturial/app/src/main/cpp/include/blend/blend.cc" "CMakeFiles/monet.dir/include/blend/blend.cc.o" "gcc" "CMakeFiles/monet.dir/include/blend/blend.cc.o.d"
+  "/home/Eighthalf/AndroidStudioProjects/ComposeTuturial/app/src/main/cpp/include/cam/cam.cc" "CMakeFiles/monet.dir/include/cam/cam.cc.o" "gcc" "CMakeFiles/monet.dir/include/cam/cam.cc.o.d"
+  "/home/Eighthalf/AndroidStudioProjects/ComposeTuturial/app/src/main/cpp/include/cam/hct.cc" "CMakeFiles/monet.dir/include/cam/hct.cc.o" "gcc" "CMakeFiles/monet.dir/include/cam/hct.cc.o.d"
+  "/home/Eighthalf/AndroidStudioProjects/ComposeTuturial/app/src/main/cpp/include/cam/hct_solver.cc" "CMakeFiles/monet.dir/include/cam/hct_solver.cc.o" "gcc" "CMakeFiles/monet.dir/include/cam/hct_solver.cc.o.d"
+  "/home/Eighthalf/AndroidStudioProjects/ComposeTuturial/app/src/main/cpp/include/cam/viewing_conditions.cc" "CMakeFiles/monet.dir/include/cam/viewing_conditions.cc.o" "gcc" "CMakeFiles/monet.dir/include/cam/viewing_conditions.cc.o.d"
+  "/home/Eighthalf/AndroidStudioProjects/ComposeTuturial/app/src/main/cpp/include/palettes/core.cc" "CMakeFiles/monet.dir/include/palettes/core.cc.o" "gcc" "CMakeFiles/monet.dir/include/palettes/core.cc.o.d"
+  "/home/Eighthalf/AndroidStudioProjects/ComposeTuturial/app/src/main/cpp/include/palettes/tones.cc" "CMakeFiles/monet.dir/include/palettes/tones.cc.o" "gcc" "CMakeFiles/monet.dir/include/palettes/tones.cc.o.d"
+  "/home/Eighthalf/AndroidStudioProjects/ComposeTuturial/app/src/main/cpp/include/scheme/scheme.cc" "CMakeFiles/monet.dir/include/scheme/scheme.cc.o" "gcc" "CMakeFiles/monet.dir/include/scheme/scheme.cc.o.d"
+  "/home/Eighthalf/AndroidStudioProjects/ComposeTuturial/app/src/main/cpp/include/utils/utils.cc" "CMakeFiles/monet.dir/include/utils/utils.cc.o" "gcc" "CMakeFiles/monet.dir/include/utils/utils.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- app/src/main/java/com/example/composetuturial/MainActivity.kt
+++ app/src/main/java/com/example/composetuturial/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.composetuturial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
@@ -22,6 +24,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetuturial.ui.theme.ComposeTuturialTheme
@@ -40,74 +43,90 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
 @Composable
 fun create_slider() {
+
     var hue by remember{ mutableStateOf(0f)}
     var chroma by remember{ mutableStateOf(0f)}
     var tone by remember{ mutableStateOf(0f)}
-    Column (modifier = Modifier.padding(20.dp),
-        verticalArrangement = Arrangement.Center){
-        Text(text = "Hue: $hue")
-        Text(text = "Chroma: $chroma")
-        Text(text = "Tone: $tone")
+    Column {
+        Column (modifier = Modifier.padding(20.dp),
+            verticalArrangement = Arrangement.Center){
+            Text(text = "Hue: $hue")
+            Text(text = "Chroma: $chroma")
+            Text(text = "Tone: $tone")
 
-    }
-
-    Column (modifier = Modifier
-        .border(1.dp, MaterialTheme.colorScheme.onSurface)
-        .height(400.dp)
-        .width(500.dp)
-        .padding(30.dp),
-        verticalArrangement = Arrangement.Center
+        }
+        Spacer(modifier = Modifier.padding(10.dp))
+        Column (modifier = Modifier
+            .border(1.dp, MaterialTheme.colorScheme.onSurface)
+            .height(400.dp)
+            .width(500.dp)
+            .padding(30.dp),
+            verticalArrangement = Arrangement.Center
         ) {
-        Column {
-            Row (modifier = Modifier.padding(20.dp),
-                horizontalArrangement = Arrangement.SpaceBetween){
-                Text(text = "Hue")
-                Spacer(Modifier.padding(horizontal = 20.dp))
-                Text(text = hue.toString())
+            Column {
+                Row (modifier = Modifier.padding(20.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween){
+                    Text(text = "Hue")
+                    Spacer(Modifier.padding(horizontal = 20.dp))
+                    Text(text = hue.toString())
+                }
+                Slider(value = hue,
+                    onValueChange = {hue = it},
+                    valueRange = 0f..360f,
+                    //modifier = Modifier.weight(1f)
+                )
             }
-            Slider(value = hue,
-                onValueChange = {hue = it},
-                valueRange = 0f..360f,
-                //modifier = Modifier.weight(1f)
-            )
-        }
-        Column() {
-            Row(modifier = Modifier.padding(10.dp),
-                horizontalArrangement = Arrangement.SpaceBetween) {
-                Text(text = "Chroma")
-                Spacer(Modifier.padding(horizontal = 10.dp))
+            Column() {
+                Row(modifier = Modifier.padding(10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween) {
+                    Text(text = "Chroma")
+                    Spacer(Modifier.padding(horizontal = 10.dp))
 
-                Text(text = chroma.toString())
+                    Text(text = chroma.toString())
+                }
+                Slider(value = chroma,
+                    onValueChange = {chroma = it},
+                    valueRange = 0f..100f,
+                    steps = 0,
+                    //modifier = Modifier.weight(1f)
+                )
             }
-            Slider(value = chroma,
-                onValueChange = {chroma = it},
-                valueRange = 0f..100f,
-                steps = 0,
-                //modifier = Modifier.weight(1f)
-            )
-        }
-        Column() {
-            Row(
-                modifier = Modifier.padding(10.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(text = "Tone")
-                Spacer(Modifier.padding(horizontal = 10.dp))
+            Column() {
+                Row(
+                    modifier = Modifier.padding(10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = "Tone")
+                    Spacer(Modifier.padding(horizontal = 10.dp))
 
-                Spacer(modifier = Modifier.padding(horizontal = 10.dp))
-                Text(text = tone.toString())
+                    Spacer(modifier = Modifier.padding(horizontal = 10.dp))
+                    Text(text = tone.toString())
+                }
+                Slider(
+                    value = tone,
+                    onValueChange = { tone = it },
+                    valueRange = 0f..100f,
+                    steps = 0,
+                    //modifier = Modifier.weight(1f)
+                )
             }
-            Slider(
-                value = tone,
-                onValueChange = { tone = it },
-                valueRange = 0f..100f,
-                steps = 0,
-                //modifier = Modifier.weight(1f)
-            )
+        }
+        Spacer(modifier = Modifier.padding(10.dp))
+        Column (modifier = Modifier.padding(20.dp)
+            .size(150.dp)
+            .border(1.dp, MaterialTheme.colorScheme.onSurface)
+            .background(color = Color(red = hue/360, green = chroma/100, blue = tone/100), shape = MaterialTheme.shapes.medium),
+            verticalArrangement = Arrangement.Center){
+
         }
     }
+
+
+
 
 }
 
